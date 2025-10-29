d1294901c559f6ba17e06393807b0406e57f029c
sobhan369arab <sobhanarab30@gmail.com>
2025-10-29 22:28:44 +0330

    feat: implement wrapper cards + skeleton when fetching data

---
+++ src/components/common/wrapper-cards/index.tsx
@@ -0,0 +1,44 @@
+import React,{ type FC }  from "react";
+import UserCard_Skeleton from "../skeleton/UserCard_Skeleton";
+import type { IProductItem } from "../../../core/types";
+import ProductCard from "../product-card";
+
+interface IProp {
+  usersData: IProductItem[] | undefined;
+  isLoading?: boolean;
+  isError?: boolean;
+}
+
+const WrapperCards: FC<IProp> = ({ usersData, isLoading, isError }) => {
+
+  const containerStyles = "flex flex-wrap justify-around gap-5";
+
+  if (isLoading || usersData === undefined) {
+    return (
+      <div className={containerStyles}>
+        {[...Array(6)].map((_, index) => (
+          <UserCard_Skeleton key={index} />
+        ))}
+      </div>
+    );
+  } else if (isError || usersData.length === 0) {
+    return (
+      <h1 className="text-center text-gray-500 text-lg mt-8">
+        User not found
+      </h1>
+    );
+  } else {
+    return (
+      <div className={containerStyles}>
+        {usersData.map((item) => (
+          <ProductCard
+            key={item.id}
+            item={item}
+          />
+        ))}
+      </div>
+    );
+  }
+};
+
+export default React.memo(WrapperCards);
--- src/components/pages/products-page/index.tsx
+++ src/components/pages/products-page/index.tsx
@@ -1,9 +1,12 @@
 import { useProducts } from "../../../core/services/APIs/get-api/GetAllProducts";
+import WrapperCards from "../../common/wrapper-cards";
 
 const ProductPage = () => {
-  const {data} = useProducts({page:1 , limit:5 , search: undefined , sortBy: undefined});
+  const {data , isLoading : productIsLoading , isError : productIsError} = useProducts({page:1 , limit:5 , search: undefined , sortBy: undefined});
   console.log(data)
-  return <div>ProductPage</div>;
+  return <div>
+    <WrapperCards usersData={data} isLoading={productIsLoading} isError={productIsError}/>
+  </div>;
 };
 
 export default ProductPage;
