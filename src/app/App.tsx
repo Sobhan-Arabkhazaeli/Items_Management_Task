d2f8243643f4c2c79cb910784877f6da23fd0686
sobhan369arab <sobhanarab30@gmail.com>
2025-10-31 22:59:54 +0330

    feat: add variable colors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,13 @@
 // ** Third Party Components
 import { RouterProvider as ReactRouterProvider } from "react-router-dom";
 import { MainRoutes } from "../config/router/MainRoute";
-import ParticlesBackground from "../core/utils/Particles";
 
 function App() {
   return (
-    <div className="w-screen p-3 font-serif">
-    <ParticlesBackground/>
-    <ReactRouterProvider router={MainRoutes} />
+    <div className="w-screen min-h-screen bg-bg font-Satoshi-Bold ">
+      <ReactRouterProvider router={MainRoutes} />
     </div>
-  )
-  
-  
+  );
 }
 
-export default App
+export default App;
